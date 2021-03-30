092b26ac175e142e227e3826acc4428d1ce58aee
enovatic <48410682+enovatic@users.noreply.github.com>
2021-03-30 14:27:50 +0200

    Deploy

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -54,6 +54,7 @@ function Chat({ user }) {
     useEffect(() => {
         getChannel();
         getMessages();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [channelId])
 
     return (
